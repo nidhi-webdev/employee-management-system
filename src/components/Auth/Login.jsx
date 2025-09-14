119667e2c7a67bc172b4481bd8ca81faa2688f4a
nidhi <kumarinidhi0594@gmail.com>
2025-09-14 15:22:44 +0200

    Login page

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const submithandler = (e) => {
+        e.preventDefault()
+        setEmail('')
+        setPassword('')
+        console.log("Form Submitted:", email, password )
+    }
+
     return (
         <div className='flex h-screen w-screen  items-center justify-center'>
             <div className='border-1 border-emerald-600 px-6 py-10 rounded-xl'>
-                <form className='flex flex-col justify-between items-center'>
-                    <input required className='border-2 rounded-full border-emerald-600 py-3 px-3 text-xl 
+                <form onSubmit={submithandler}
+                    className='flex flex-col justify-between items-center'>
+                    <input required 
+                    value={email}
+                    onChange={(e) => {
+                        setEmail(e.target.value)
+                    }}
+                    className='border-2 rounded-full border-emerald-600 py-3 px-3 text-xl 
                     outline-none placeholder:text-gray-400 mb-5 '
                         type='email' placeholder='Enter Your email' />
-                    <input required className='border-2 rounded-full border-emerald-600 py-3 px-3 text-xl
+                    <input 
+                    value={password}
+                    onChange={(e) => {
+                        setPassword(e.target.value)
+                    }}
+                    required className='border-2 rounded-full border-emerald-600 py-3 px-3 text-xl
                     outline-none placeholder:text-gray-400 mb-5'
                         type='password' placeholder='Enter your password' />
                     <button className='border-2 rounded-full bg-emerald-600 py-3 px-5 text-xl
                     outline-none text-emerald-100 mt-5'>
-                        Log in 
+                        Log in
                     </button>
                 </form>
             </div>
