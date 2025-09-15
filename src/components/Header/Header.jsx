625e12bbebf6c2049475af3a89c4a90b8d89f009
nidhi <kumarinidhi0594@gmail.com>
2025-09-15 14:05:30 +0200

    Header2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='flex items-end justify-between'>
-      <h1 className='text-2xl font-medium'> Hello 👋 <br />Nidhi  </h1>
-      <button> Log Out </button>
+      <h1 className='text-2xl font-medium'> Hello  <br /> <span className='text-3xl font-semibold'> Nidhi 👋 </span>  </h1>
+      <button className='bg-red-600 text-lg font-medium px-5 py-2 rounded-small'> Log Out </button>
     </div>
   )
 }
