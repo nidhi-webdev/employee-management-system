b460288f6314d146fb356d1717a84a2b02ca027b
nidhi <kumarinidhi0594@gmail.com>
2025-09-16 20:25:51 +0200

    Task List component

---
+++ src/components/Others/TaskListCount.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TaskListCount = () => {
+  return (
+    <div className='flex justify-between screen gap-5'>
+        <div className='h-40 w-1/2 bg-red-400'>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default TaskListCount
