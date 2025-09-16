5a27b54ccbee61995ad89e5b2664e2c940dc3a13
nidhi <kumarinidhi0594@gmail.com>
2025-09-16 20:35:36 +0200

    css

--- src/components/Others/TaskListCount.jsx
+++ src/components/Others/TaskListCount.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const TaskListCount = () => {
-  return (
-    <div className='flex mt-10 justify-between screen gap-5'>
-        <div className=' rounded-xl w-[45%] py-5 px-10  bg-red-400'>
+    return (
+        <div className='flex mt-10 justify-between screen gap-5'>
+            <div className=' rounded-xl w-[45%] py-5 px-10  bg-red-400'>
+                <h2> 0 </h2>
+            </div>
 
         </div>
-      
-    </div>
-  )
+    )
 }
 
 export default TaskListCount
