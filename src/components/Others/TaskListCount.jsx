8dabf50888d46835c995e921efc8055beaccc22c
nidhi <kumarinidhi0594@gmail.com>
2025-09-16 20:36:50 +0200

    css1

--- src/components/Others/TaskListCount.jsx
+++ src/components/Others/TaskListCount.jsx
@@ -5,6 +5,7 @@ const TaskListCount = () => {
         <div className='flex mt-10 justify-between screen gap-5'>
             <div className=' rounded-xl w-[45%] py-5 px-10  bg-red-400'>
                 <h2 className='text-3xl font-semibold'> 0 </h2>
+                <h3> New task </h3>
             </div>
 
         </div>
