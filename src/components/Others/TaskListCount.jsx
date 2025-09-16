55949cba8c4b5287042d096f179c56f5bd520ad1
nidhi <kumarinidhi0594@gmail.com>
2025-09-16 20:36:22 +0200

    css

--- src/components/Others/TaskListCount.jsx
+++ src/components/Others/TaskListCount.jsx
@@ -4,7 +4,7 @@ const TaskListCount = () => {
     return (
         <div className='flex mt-10 justify-between screen gap-5'>
             <div className=' rounded-xl w-[45%] py-5 px-10  bg-red-400'>
-                <h2> 0 </h2>
+                <h2 className='text-3xl font-semibold'> 0 </h2>
             </div>
 
         </div>
