e13dfe3e3f0a1262ffc79eccd51463feacc8456c
nidhi <kumarinidhi0594@gmail.com>
2025-09-17 16:55:15 +0200

    Tasklist

---
+++ src/TaskList/TaskList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div className='h-[55vh] w-full bg-red-500 mt-10'>
+     
+    </div>
+  )
+}
+
+export default TaskList
--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../Others/Header'
 import TaskListCount from '../Others/TaskListCount'
+import TaskList from '../../TaskList/TaskList'
 
 const EmployeeDashboard = () => {
   return (
     <div>
       <Header />
       <TaskListCount />
+      <TaskList />
     </div>
   )
 }
