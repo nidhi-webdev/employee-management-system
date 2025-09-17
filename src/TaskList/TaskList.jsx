b5860a19a3de1e90c98897a2b1fd955ed7981449
nidhi <kumarinidhi0594@gmail.com>
2025-09-17 17:27:51 +0200

    Tasklist

--- src/TaskList/TaskList.jsx
+++ src/TaskList/TaskList.jsx
@@ -1,11 +1,50 @@
 import React from 'react'
 
 const TaskList = () => {
-  return (
-    <div className='h-[55vh] w-full bg-red-500 mt-10'>
-     
-    </div>
-  )
+    return (
+        <div id="tasklist" className='h-[55vh] w-full  mt-10 flex items-center justify-start gap-5 flex-nowrap overflow-x-auto'>
+            <div className='h-full w-[300px] p-5 bg-red-400 rounded-xl flex-shrink-0'>
+                <div className='flex justify-between items-center'>
+                    <h3 className='bg-red-600 text-sm px-3 py-1 rounded'> High </h3>
+                    <h4> 17 Sep 2025 </h4>
+                </div>
+                <h2 className='mt-5 text-2xl font-semibold'> Make a react project </h2>
+                <p className='text-sm mt-3'>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste magnam voluptatem voluptas sint. Enim, ex a quaerat ad labore alias libero voluptate fugit voluptatem odio at, tempore quasi molestias voluptates.
+                </p>
+            </div>
+            <div className='h-full w-[300px] p-5 bg-green-400 rounded-xl flex-shrink-0'>
+                <div className='flex justify-between items-center'>
+                    <h3 className='bg-red-600 text-sm px-3 py-1 rounded'> High </h3>
+                    <h4> 17 Sep 2025 </h4>
+                </div>
+                <h2 className='mt-5 text-2xl font-semibold'> Make a react project </h2>
+                <p className='text-sm mt-3'>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste magnam voluptatem voluptas sint. Enim, ex a quaerat ad labore alias libero voluptate fugit voluptatem odio at, tempore quasi molestias voluptates.
+                </p>
+            </div>
+            <div className='h-full w-[300px] p-5 bg-blue-400 rounded-xl flex-shrink-0'>
+                <div className='flex justify-between items-center'>
+                    <h3 className='bg-red-600 text-sm px-3 py-1 rounded'> High </h3>
+                    <h4> 17 Sep 2025 </h4>
+                </div>
+                <h2 className='mt-5 text-2xl font-semibold'> Make a react project </h2>
+                <p className='text-sm mt-3'>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste magnam voluptatem voluptas sint. Enim, ex a quaerat ad labore alias libero voluptate fugit voluptatem odio at, tempore quasi molestias voluptates.
+                </p>
+            </div>
+            <div className='h-full w-[300px] p-5 bg-yellow-400 rounded-xl flex-shrink-0'>
+                <div className='flex justify-between items-center'>
+                    <h3 className='bg-red-600 text-sm px-3 py-1 rounded'> High </h3>
+                    <h4> 17 Sep 2025 </h4>
+                </div>
+                <h2 className='mt-5 text-2xl font-semibold'> Make a react project </h2>
+                <p className='text-sm mt-3'>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste magnam voluptatem voluptas sint. Enim, ex a quaerat ad labore alias libero voluptate fugit voluptatem odio at, tempore quasi molestias voluptates.
+                </p>
+            </div>
+        </div>
+    )
 }
 
 export default TaskList
