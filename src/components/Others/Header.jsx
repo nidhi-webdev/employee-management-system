05778f2bea2f058313605c01c2f4343815a1d3dd
nidhi <kumarinidhi0594@gmail.com>
2025-09-21 21:00:35 +0200

    Design

--- src/components/Others/Header.jsx
+++ src/components/Others/Header.jsx
@@ -8,7 +8,7 @@ const Header = ({handleLogout}) => {
   return (
     <div className='flex items-end justify-between m-10'>
       <h1 className='text-2xl font-medium text-white'> Hello  <br /> <span className='text-3xl font-semibold'> Nidhi 👋 </span>  </h1>
-      <button className='bg-red-600 text-lg font-medium px-5 py-2 rounded-small cursor-pointer' onClick={handleLogout}> Log Out </button>
+      <button className='bg-red-600 text-lg font-medium px-5 py-2 rounded-xl cursor-pointer' onClick={handleLogout}> Log Out </button>
     </div>
   )
 }
