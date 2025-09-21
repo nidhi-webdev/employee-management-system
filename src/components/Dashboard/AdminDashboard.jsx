739b801f6febc2b85436a233da49fc087d4628e3
nidhi <kumarinidhi0594@gmail.com>
2025-09-21 20:55:56 +0200

    Handling Logout Functionality

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -3,10 +3,10 @@ import Header from '../Others/Header'
 import CreateTask from '../Others/CreateTask'
 import AllTask from '../Others/AllTask'
 
-const AdminDashboard = () => {
+const AdminDashboard = ({ handleLogout }) => {
     return (
         <div className='h-screen w-full p-7'>
-            <Header />
+            <Header handleLogout={handleLogout} />
            <CreateTask />
            <AllTask />
         </div>
