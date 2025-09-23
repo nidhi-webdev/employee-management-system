910d1c9e3da2f073c6600e8d585b2c44e1efa84a
nidhi <kumarinidhi0594@gmail.com>
2025-09-23 20:32:35 +0200

    formatting

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -4,7 +4,6 @@ import CreateTask from '../Others/CreateTask'
 import AllTask from '../Others/AllTask'
 
 const AdminDashboard = ({ handleLogout, employee }) => {
-    console.log("From Admin Dashboard:", employee)
     return (
         <div className='h-screen w-full p-7'>
             <Header handleLogout={handleLogout} employee={employee} />
