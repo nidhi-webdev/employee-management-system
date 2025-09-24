8421f05ce498e450bb0c2c7a996efd8b63f42605
nidhi <kumarinidhi0594@gmail.com>
2025-09-24 21:45:23 +0200

    Task List Count

--- src/components/Others/TaskListCount.jsx
+++ src/components/Others/TaskListCount.jsx
@@ -5,24 +5,24 @@ const TaskListCount = ({employee}) => {
     return (
         
         <div className='flex mt-10 justify-between screen gap-5'>
-            <div className=' rounded-xl w-[45%] py-5 px-10  bg-red-400'>
-                <h2 className='text-3xl font-semibold'> 0 </h2>
-                <h3> New task </h3>
+            <div className=' rounded-xl w-[45%] py-5 px-10 bg-yellow-400 '>
+                <h2 className='text-3xl font-semibold'> {employee.tastCounts.active} </h2>
+                <h3> Accepted task </h3>
             </div>
 
              <div className=' rounded-xl w-[45%] py-5 px-10  bg-green-400'>
-                <h2 className='text-3xl font-semibold'> 0 </h2>
-                <h3> New task </h3>
+                <h2 className='text-3xl font-semibold'> {employee.tastCounts.completed} </h2>
+                <h3> Completed Task </h3>
             </div>
 
              <div className=' rounded-xl w-[45%] py-5 px-10  bg-blue-400'>
-                <h2 className='text-3xl font-semibold'> 0 </h2>
+                <h2 className='text-3xl font-semibold'> {employee.tastCounts.newTask} </h2>
                 <h3> New task </h3>
             </div>
 
-             <div className=' rounded-xl w-[45%] py-5 px-10  bg-yellow-400'>
-                <h2 className='text-3xl font-semibold'> 0 </h2>
-                <h3> New task </h3>
+             <div className=' rounded-xl w-[45%] py-5 px-10  bg-red-400'>
+                <h2 className='text-3xl font-semibold'> {employee.tastCounts.active} </h2>
+                <h3> Failed Task </h3>
             </div>
 
         </div>
