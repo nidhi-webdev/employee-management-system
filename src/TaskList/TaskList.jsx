444490f9354159d73d5965c1b50b45fdb7601f3a
nidhi <kumarinidhi0594@gmail.com>
2025-09-27 12:21:00 +0200

    Complete task

--- src/TaskList/TaskList.jsx
+++ src/TaskList/TaskList.jsx
@@ -1,22 +1,14 @@
 import React from 'react'
 import AcceptTask from '../TaskList/AcceptTask'
 import NewTask from './NewTask'
+import CompleteTask from '../TaskList/CompleteTask'
 
 const TaskList = (employee) => {
     return (
         <div id="tasklist" className='h-[55vh] w-full  mt-10 flex items-center  gap-20 flex-nowrap overflow-x-auto'>
            <AcceptTask />
            <NewTask />
-            <div className=' w-[300px] p-5 bg-blue-400 rounded-xl flex-shrink-0'>
-                <div className='flex justify-between items-center'>
-                    <h3 className='bg-red-600 text-sm px-3 py-1 rounded'> High </h3>
-                    <h4> 17 Sep 2025 </h4>
-                </div>
-                <h2 className='mt-5 text-2xl font-semibold'> Make a react project </h2>
-                <p className='text-sm mt-3'>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste magnam voluptatem voluptas sint. Enim, ex a quaerat ad labore alias libero voluptate fugit voluptatem odio at, tempore quasi molestias voluptates.
-                </p>
-            </div>
+            <CompleteTask  />
             <div className=' w-[300px] p-5 bg-yellow-400 rounded-xl flex-shrink-0'>
                 <div className='flex justify-between items-center'>
                     <h3 className='bg-red-600 text-sm px-3 py-1 rounded'> High </h3>
