68e65daa0e51efe9b4ddca185240d7178f6727e0
nidhi <kumarinidhi0594@gmail.com>
2025-09-29 20:40:14 +0200

    failed Task

--- src/TaskList/FailedTask.jsx
+++ src/TaskList/FailedTask.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const FailedTask = ({task}) => {
+const FailedTask = ({ task }) => {
     return (
         <div className=' w-[300px] p-5 bg-red-400 rounded-xl flex-shrink-0'>
             <div className='flex justify-between items-center'>
@@ -8,7 +8,7 @@ const FailedTask = ({task}) => {
                 <h4> {task.taskDate} </h4>
             </div>
             <h2 className='mt-5 text-2xl font-semibold'> {task.taskTitle} </h2>
-            <p className='text-sm mt-3'> {task.taskDescription}   
+            <p className='text-sm mt-3'> {task.taskDescription}
             </p>
             <div className='w-full'>
                 <button className='w-full'> Falied </button>
