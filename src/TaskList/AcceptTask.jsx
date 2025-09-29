74a317453ac0bb5d5eff22c968da1ecd6f4f0634
nidhi <kumarinidhi0594@gmail.com>
2025-09-29 20:32:32 +0200

    Accepted Task

--- src/TaskList/AcceptTask.jsx
+++ src/TaskList/AcceptTask.jsx
@@ -7,7 +7,7 @@ const AcceptTask = ({data, task}) => {
     <div>
        <div className=' w-[300px] p-5 bg-yellow-400 rounded-xl flex-shrink-0'>
                 <div className='flex justify-between items-center'>
-                    <h3 className='bg-red-600 text-sm px-3 py-1 rounded'> High </h3>
+                    <h3 className='bg-red-600 text-sm px-3 py-1 rounded'> {task.category} </h3>
                     <h4> {task.taskDate} </h4>
                 </div>
                 <h2 className='mt-5 text-2xl font-semibold'> {task.taskTitle} </h2>
