79ce03df825997e90af228f099126570edfa1f70
nidhi <kumarinidhi0594@gmail.com>
2025-09-29 20:29:04 +0200

    Accessing the data in Accepted task which is coming form TaskList

--- src/TaskList/AcceptTask.jsx
+++ src/TaskList/AcceptTask.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
-const AcceptTask = ({data}) => {
-  console.log("From Accepted task", data)
+const AcceptTask = ({data, task}) => {
+  console.log("From Accepted task", data?.tasks)
+  console.log("From task", task)
   return (
     <div>
        <div className=' w-[300px] p-5 bg-yellow-400 rounded-xl flex-shrink-0'>
@@ -9,7 +10,7 @@ const AcceptTask = ({data}) => {
                     <h3 className='bg-red-600 text-sm px-3 py-1 rounded'> High </h3>
                     <h4> 17 Sep 2025 </h4>
                 </div>
-                <h2 className='mt-5 text-2xl font-semibold'> Make a react project </h2>
+                <h2 className='mt-5 text-2xl font-semibold'> {task.taskTitle} </h2>
                 <p className='text-sm mt-3'>
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste magnam voluptatem voluptas sint. Enim, ex a quaerat ad labore alias libero voluptate fugit voluptatem odio at, tempore quasi molestias voluptates.
                 </p>
