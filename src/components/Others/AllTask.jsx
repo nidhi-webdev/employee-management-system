d42ed348833db85fc4c2b544a5c71f8967b42178
nidhi <kumarinidhi0594@gmail.com>
2025-09-30 19:37:47 +0200

    All Task

--- src/components/Others/AllTask.jsx
+++ src/components/Others/AllTask.jsx
@@ -2,32 +2,35 @@ import React, { useContext } from 'react'
 import { AuthContext } from '../../context/AuthProvider'
 
 const AllTask = () => {
-   const authdata =  useContext(AuthContext)
-   console.log("All Task", authdata.employees)
+    const authdata = useContext(AuthContext)
+    console.log("All Task", authdata.employees)
 
     return (
         <div className='bg-[#1c1c1c] p-5 mt-5 rounded overflow-auto h-50'>
-            <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded text-white'>
+            {authdata.employees.map(function () {
+                return <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded text-white'>
+                    <h2> Nidhi </h2>
+                    <h3> Make a UI Design </h3>
+                    <h5> Status </h5>
+                </div>
+            })}
+
+            <div className='bg-blue-400 mb-2 py-2 px-4 flex justify-between rounded text-white'>
                 <h2> Nidhi </h2>
                 <h3> Make a UI Design </h3>
                 <h5> Status </h5>
             </div>
-             <div className='bg-blue-400 mb-2 py-2 px-4 flex justify-between rounded text-white'>
+            <div className='bg-green-400 mb-2 py-2 px-4 flex justify-between rounded text-white'>
                 <h2> Nidhi </h2>
                 <h3> Make a UI Design </h3>
                 <h5> Status </h5>
             </div>
-             <div className='bg-green-400 mb-2 py-2 px-4 flex justify-between rounded text-white'>
+            <div className='bg-yellow-400 mb-2 py-2 px-4 flex justify-between rounded text-white'>
                 <h2> Nidhi </h2>
                 <h3> Make a UI Design </h3>
                 <h5> Status </h5>
             </div>
-             <div className='bg-yellow-400 mb-2 py-2 px-4 flex justify-between rounded text-white'>
-                <h2> Nidhi </h2>
-                <h3> Make a UI Design </h3>
-                <h5> Status </h5>
-            </div>
-             <div className='bg-purple-400 mb-2 py-2 px-4 flex justify-between rounded text-white'>
+            <div className='bg-purple-400 mb-2 py-2 px-4 flex justify-between rounded text-white'>
                 <h2> Nidhi </h2>
                 <h3> Make a UI Design </h3>
                 <h5> Status </h5>
