92c04eb3ee08a7a7f2977d4488cf98addabb7896
nidhi <kumarinidhi0594@gmail.com>
2025-09-30 19:38:38 +0200

    All Task formatt

--- src/components/Others/AllTask.jsx
+++ src/components/Others/AllTask.jsx
@@ -14,27 +14,6 @@ const AllTask = () => {
                     <h5> Status </h5>
                 </div>
             })}
-
-            <div className='bg-blue-400 mb-2 py-2 px-4 flex justify-between rounded text-white'>
-                <h2> Nidhi </h2>
-                <h3> Make a UI Design </h3>
-                <h5> Status </h5>
-            </div>
-            <div className='bg-green-400 mb-2 py-2 px-4 flex justify-between rounded text-white'>
-                <h2> Nidhi </h2>
-                <h3> Make a UI Design </h3>
-                <h5> Status </h5>
-            </div>
-            <div className='bg-yellow-400 mb-2 py-2 px-4 flex justify-between rounded text-white'>
-                <h2> Nidhi </h2>
-                <h3> Make a UI Design </h3>
-                <h5> Status </h5>
-            </div>
-            <div className='bg-purple-400 mb-2 py-2 px-4 flex justify-between rounded text-white'>
-                <h2> Nidhi </h2>
-                <h3> Make a UI Design </h3>
-                <h5> Status </h5>
-            </div>
         </div>
     )
 }
