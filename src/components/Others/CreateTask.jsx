265b4c664b8b7b4d91db426bbc07da5d8dfc63af
nidhi <kumarinidhi0594@gmail.com>
2025-10-02 22:01:33 +0200

    Created task

--- src/components/Others/CreateTask.jsx
+++ src/components/Others/CreateTask.jsx
@@ -27,22 +27,42 @@ const CreateTask = () => {
                     <div className='w-1/2'>
                         <div>
                             <h3 className='text-sm text-gray-300 mb-0.5'> Task Tittle </h3>
-                            <input type='text' placeholder='Make a UI Design' className='text-sm text-white py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4 placeholder-gray-400 '
+                            <input 
+                            value={titleTask} 
+                            onChange={(e) => {
+                              setTitleTask(e.target.value)
+                            }}
+                            type='text' placeholder='Make a UI Design' className='text-sm text-white py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4 placeholder-gray-400 '
                             />
                         </div>
                         <div>
                             <h3 className='text-sm text-gray-300 mb-0.5'> Date </h3>
-                            <input type='date' className='text-sm text-white py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4 placeholder-gray-400' />
+                            <input 
+                            value={date}
+                            onChange={(e) => {
+                              setDate(e.target.value)
+                            }}
+                            type='date' className='text-sm text-white py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4 placeholder-gray-400' />
                         </div>
 
                         <div>
                             <h3 className='text-sm text-gray-300 mb-0.5'> Asign To  </h3>
-                            <input type='text' placeholder='Employee Name' className='text-sm text-white py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4 placeholder-gray-400' />
+                            <input 
+                            value={asignTo} 
+                            onChange={(e) => {
+                              setAsignTo(e.target.value)
+                            }}
+                            type='text' placeholder='Employee Name' className='text-sm text-white py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4 placeholder-gray-400' />
                         </div>
 
                         <div>
                             <h3 className='text-sm text-gray-300 mb-0.5'> Category </h3>
-                            <input type='text' placeholder='Design, Dev etc' className='text-sm text-white py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4 placeholder-gray-400' />
+                            <input 
+                            value={category}
+                            onChange={(e) => {
+                              setCategory(e.target.value)
+                            }}
+                            type='text' placeholder='Design, Dev etc' className='text-sm text-white py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4 placeholder-gray-400' />
                         </div>
                     </div>
 
